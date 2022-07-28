58cc8fc9ca1d16c0177645b3e040cdab94c8b558
plur <plur@live.ru>
2022-07-28 09:44:11 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      let middle = Math.round((this.min + this.max) / 2);
+      return middle;
     }
-
+  
     lower() {
-
+      this.max = this.guess();
     }
-
+  
     greater() {
-
+      this.min = this.guess();
     }
-}
-
+  }
+  
 module.exports = GuessingGame;
+
+
